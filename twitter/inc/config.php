26626875b4afb93738ad9cec2a773fd87f92c9c0
Chirag Ramani <chiragramani145@gmail.com>
2017-08-17 22:34:00 +0530

    Add files via upload

---
+++ twitter/inc/config.php
@@ -0,0 +1,4 @@
+<?php
+
+define("CONSUMER_KEY", "lryz094ovqNW1a8CQkIUt3AmL");
+define("CONSUMER_SECRET", "YO2NlXOvr9cxi51XKR0uEdo3zQf7pAj91ZkYt5a7ebGZvxo6hy");
---
+++ twitter/login.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+require "twitteroauth-master/autoload.php";
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+$consumer_key = "lryz094ovqNW1a8CQkIUt3AmL";
+$consumer_secret = "YO2NlXOvr9cxi51XKR0uEdo3zQf7pAj91ZkYt5a7ebGZvxo6hy";
+$access_token = "3848701513-qe017FEmF0mDMLPgU9a5WcX5Jaektft2w8QOtwP";
+$access_token_secret = "L92qF9nnOsehV880S8knI55NsXN9q6EVjq9Nio1nPPa1e";
+$connection = new TwitterOAuth($consumer_key, $consumer_secret);
+
+$request_token = $connection->oauth(
+    'oauth/request_token',
+    [
+        'oauth_callback' => 'http://ramanicare.com/twitter/hello'
+    ]
+);
+
+if ($connection->getLastHttpCode() != 200) {
+    throw new \Exception('There was a problem performing this request');
+}
+ 
+ $_SESSION['oauth_token'] = $request_token['oauth_token'];
+$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+ 
+ $url = $connection->url(
+    'oauth/authorize',
+ 
+     [
+        'oauth_token' => $request_token['oauth_token']
+    ]
+);
+// echo $url;
+// and redirect
+header('Location: '. $url);
